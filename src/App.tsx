42f943d76f0551e6b7fffa6a11536d59c85cb97e
aschmidt20 <andrew@aave.com>
2024-04-03 20:57:10 -0500

    feat: working implementation of action calldata

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,8 @@ function App() {
   const [postActionData, setPostActionData] = useState("");
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [publication, setPublication] = useState<any | null>(null);
+  const [loadingInitialize, setLoadingInitialize] = useState<boolean>(false);
+  const [loadingAct, setLoadingAct] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchPublicationData = async () => {
@@ -29,34 +31,40 @@ function App() {
       if (postIdIndex !== -1 && postIdIndex + 1 < parts.length) {
         pubId = parts[postIdIndex + 1];
       }
-      const clientPublication = await lensClient.publication.fetch({
-        forId: pubId,
-      });
-      setPublication(clientPublication);
+      if (pubId !== "") {
+        const clientPublication = await lensClient.publication.fetch({
+          forId: pubId,
+        });
+        setPublication(clientPublication);
+      }
     };
     fetchPublicationData();
   }, [postLink]);
 
   const handleDetect = async () => {
     try {
+      setLoadingInitialize(true);
       const result = await nftOpenActionKit.detectAndReturnCalldata(nftLink);
       setActionData(result || "No action data found");
     } catch (err) {
       setActionData("Error: " + err);
     }
+    setLoadingInitialize(false);
   };
 
   const handlePostAction = async () => {
     try {
       if (publication) {
+        const [profileHex, pubHex] = publication.id.split("-");
+        setLoadingAct(true);
         const result = await nftOpenActionKit.actionDataFromPost(
           {
-            profileId: publication.by.id,
+            profileId: parseInt(profileHex, 16).toString(),
             actionModules: [publication.openActionModules[0].contract.address],
             actionModulesInitDatas: [
               publication.openActionModules[0].initializeCalldata,
             ],
-            pubId: publication.id,
+            pubId: parseInt(pubHex, 16).toString(),
           },
           "10859",
           "0x354cd122a1b3ebf102306f6bccccf8abebaff708",
@@ -65,11 +73,15 @@ function App() {
           BigInt(1),
           "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270"
         );
-        setPostActionData(JSON.stringify(result) || "No action data found");
+        const resultStr = JSON.stringify(result, (_, value) =>
+          typeof value === "bigint" ? value.toString() : value
+        );
+        setPostActionData(resultStr || "No action data found");
       }
     } catch (err) {
       setPostActionData("Error: " + err);
     }
+    setLoadingAct(false);
   };
 
   return (
@@ -82,7 +94,10 @@ function App() {
           onChange={(e) => setNftLink(e.target.value)}
           placeholder="Enter NFT link"
         />
-        <button onClick={handleDetect}>Detect</button>
+        <button className="query" onClick={handleDetect}>
+          Detect
+        </button>
+        {loadingInitialize && <div className="loader" />}
         <div className="result">{actionData}</div>
       </div>
       <div className="section">
@@ -93,7 +108,10 @@ function App() {
           onChange={(e) => setPostLink(e.target.value)}
           placeholder="Enter hey.xyz post link"
         />
-        <button onClick={handlePostAction}>Get Action Data</button>
+        <button className="query" onClick={handlePostAction}>
+          Get Action Data
+        </button>
+        {loadingAct && <div className="loader" />}
         <div className="result">{postActionData}</div>
       </div>
     </div>
